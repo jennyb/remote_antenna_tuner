487a78a460c43652147865ffeb8ccccaec9094b7
Jenny bailey <jennyb@jsquared.co.uk>
2023-09-08 19:08:45 +0100

    Server testing

--- remote_antenna_tuner.py
+++ remote_antenna_tuner.py
@@ -214,11 +214,11 @@ def webpage(state):
 def serve(connection):
     #Start a web server
     state = 'OFF'
-    # ToDo remove unwanted temperaturetemperature = 0
     while True:
         client = connection.accept()[0]
         request = client.recv(1024)
         request = str(request)
+        print('HTTP client request')
         try:
             request = request.split()[1]
         except IndexError:
