0025e9aea7d2877ae4124b6fa2fc913f982c7b6c
Jenny bailey <jennyb@jsquared.co.uk>
2023-09-26 11:52:07 +0100

    Devel (#2)
    * Trivial change
    
    * removed the memory class
    
    * added display handling for local control og ATU
    
    * Update display_handling.py
    
    Turning this into a class
    
    * Update display_handling.py
    
    buttons are now handled by the main code.
    
    * Added Interrupt control from display switches
    
    * Added some ToDos and isr for buttons
    
    * Not using pimoroni button, use pin instead
    
    * print network SSIDs to screen
    
    * display list of SSIDs
    
    * fix wifi connect bug
    
    if there are less than 0 wifi ssids, then wait and try again. If there are 1-6 the display them
    
    * more hardware definitions
    
    * fix bug number of networks
    
    * sort the SSID results
    
    * fix zero network display bug
    
    * Server testing
    
    * onboard LED. Test pin on boot
    
    Onboard LED on when logged in to WiFi network as per Phils request
    Test button A on bootup to exit main. Useful for upgrading

--- remote_antenna_tuner.py
+++ remote_antenna_tuner.py
@@ -5,6 +5,50 @@ from machine import Pin
 import os
 import json
 from storage import Storage
+import display_handling 
+
+# Hardware
+# Display LED R 9
+# Display LED B 10
+# Display LED B 11
+# Display LCD_TE 27
+# Display Backlight En 26
+# Display LCD_MOSI 25
+# Display LCD_SCLK 24
+# Display LCD_CS 22
+# Display LCD_DC 21
+button_a = Pin(12, mode=Pin.IN, pull=Pin.PULL_UP)
+button_b = Pin(13, mode=Pin.IN, pull=Pin.PULL_UP)
+button_x = Pin(14, mode=Pin.IN, pull=Pin.PULL_UP)
+button_y = Pin(15, mode=Pin.IN, pull=Pin.PULL_UP)
+stepper_enable = Pin(3, Pin.OUT, value=1 ) 	# active low. The current cnc interface board has one common stepper motor driver enble line.
+                                            # this is the current cnc interface https://www.az-delivery.uk/products/az-delivery-cnc-shield-v3
+                                            # My new PCB will have one enable for each stepper motor driver
+
+# transmitter step Pin(9)
+# transmitter dir  Pin(6)
+# antenna step     Pin(8)
+# antenna dir      Pin(5)
+# inductance step  Pin(7)
+# inductance dir   Pin(4)
+led = Pin("LED", Pin.OUT)
+
+
+
+
+
+def button_a_isr(pin):
+    user_display.button_a_pressed()
+    #pin_led.value(not pin_led.value())
+    
+def button_b_isr(pin):
+    user_display.button_b_pressed()    
+
+def button_x_isr(pin):
+    user_display.button_x_pressed()
+
+def button_y_isr(pin):
+    user_display.button_y_pressed()
 
 class Stepper:
     def __init__(self, name:str, step_pin:Pin, dir_pin:Pin, counter=0):
@@ -18,78 +62,83 @@ class Stepper:
     def rotate(self, direction, steps):
         print (f'Rotating : name: {self.name}, direction: {direction}, steps: {steps}')
         self.dir_pin.value(direction)
-        stepper_enable.value(0)
+        stepper_enable.value(0) # set the hardware enable to be active low 
         sleep(0.01)
    
         for step in range(0, steps):
+            if (direction ):
+                self.counter += 1
+            else :
+                # this is where we stop the stepper motor going to a negative value
+                # for a rollercoaster tuning inductor this would just hit the zero end stop and be meaningless
+                # for a variable capacitor, they are only useful between 0 and 180 degrees.
+                # Outside this they are just repeating the same capacitance 
+                if self.counter > 0: 
+                    self.counter -= 1
+                else:
+                    break # drop out of the For loop, because we do not want to reverse the motor beyond/before 0
             self.step_pin.value(1)
             sleep(0.01)
             self.step_pin.value(0)
             sleep(0.01)
-            if (direction ):
-                self.counter += 1
-            else :
-                self.counter -= 1
+
+                    
         sleep(0.1)
-        stepper_enable.value(1)
+        stepper_enable.value(1) # set the hardware enable to be inactive high
         
     def set(self, new_position:int ):
         pass
 
-class Memory:
-    def __init__(self, steps1:int, steps2:int, steps3:int):
-        # ToDo - read the initial value from a file holding the last readings before being switched off
-        # self.name = name
-        self.store(steps1, steps2, steps3)
-        
-    def store(self, steps1:int, steps2:int, steps3:int):
-        self.steps1 = steps1
-        self.steps2 = steps2
-        self.steps3 = steps3
-        pass
-        
-    def recall(self):
-        return [ self.steps1,self.steps2,self.steps3]
-    
-# the pi pico is connected to a cheap CNC driver boad
-# pins X,Y,Z step and X,Y,Z dir and one enable pin for all
-# are connected to 9,8,7 step and 6,5,4 direction and 3 enable
 
 motors = [Stepper('transmitter', Pin(9), Pin(6)),
           Stepper('antenna', Pin(8), Pin(5)),
           Stepper('inductance', Pin(7), Pin(4))]
 
-# Each memory holds 3 step counts. Currently one for each transmitter, antenna and inductance
-# ToDo - I've had to do this longhand, need to make the number of memories configurable 
-memories = [Memory(20,78,100),
-            Memory(15,65,50),
-            Memory(0,0,0),
-            Memory(0,0,0),
-            Memory(0,0,0),
-            Memory(0,0,0),
-            Memory(0,0,0),            
-            Memory(0,0,0)]
-
-stepper_enable = Pin(3, Pin.OUT, value=1 )
+# ToDo - Make the number of memories configurable 
 
 def connect():
     #Connect to WLAN
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     
-    #List all available networks
-    networks = wlan.scan() # list with tupples with 6 fields ssid, bssid, channel, RSSI, security, hidden    
-    for w in networks:
-        print(f'SSID: {w[0]},\t\t Channel: {w[2]},\t signal strength: {w[3]}' )
+    
+    # List all available networks
+    # if no networks are available, wait then try again. 
+    # networks is an array of tupples with 6 fields ssid, bssid, channel, RSSI, security, hidden 
+    while True :
+        networks = wlan.scan()
 
+        number_of_networks = len(networks)     
+        if number_of_networks == 0:
+            # make an empty network to display that there is no network.
+            networks = [('None',0,0,0,0,0)]
+            user_display.display_networks(networks)
+            sleep(10)
+        else :
+            # at least one network has been found ( it may not include the one that we want )      
+            for w in networks:
+                print(f'SSID: {w[0]},\t\t Channel: {w[2]},\t signal strength: {w[3]}\n' )
+            user_display.display_networks(networks)
+            break
+    
 
     print( f'Connecting to SSID: {nv_data.get_ssid()}')
     wlan.connect(str(nv_data.get_ssid()),  str(nv_data.get_password()) )
     
+    connection_timeout = 10      # sometimes the wlan keep trying and never gets a connection. Reset the Pico after 10 tries
     while wlan.isconnected() == False:
-        print('Waiting for connection...')
+        connection_timeout -= 1
+        if not connection_timeout :
+            print('reboot forced due to no wlan connection')
+            machine.reset()    
+        print(f'Waiting for IP address. {connection_timeout} seconds left')
         sleep(1)
+      
     ip = wlan.ifconfig()[0]
+    # turn on the onboard LED to show network connection - useful with no display
+    led.on()
+    user_display.set_ip(ip)
+    # update display to show IP AND signal level
     print(f'Connected on {ip}')
     return ip
 
@@ -102,7 +151,8 @@ def open_socket(ip):
     connection.listen(1)
     return connection
 
-def webpage(temperature, state):
+
+def webpage(state):
     #Template HTML
     html = f"""
             <!DOCTYPE html>
@@ -139,7 +189,25 @@ def webpage(temperature, state):
             <input type="submit" name='m_0_0_3' value="Recall 80m High" />
             <input type="submit" name='m_0_1_2' value="Save 80m Low" />
             <input type="submit" name='m_0_1_3' value="Save 80m High" />
-            </form> 
+            </form>
+            <form>
+            <input type="submit" name='m_0_0_4' value="Recall 60m Low" />
+            <input type="submit" name='m_0_0_5' value="Recall 60m High" />
+            <input type="submit" name='m_0_1_4' value="Save 60m Low" />
+            <input type="submit" name='m_0_1_5' value="Save 60m High" />
+            </form>
+            <form>
+            <input type="submit" name='m_0_0_6' value="Recall 40m Low" />
+            <input type="submit" name='m_0_0_7' value="Recall 40m High" />
+            <input type="submit" name='m_0_1_6' value="Save 40m Low" />
+            <input type="submit" name='m_0_1_7' value="Save 40m High" />
+            </form>
+            <form>
+            <input type="submit" name='m_0_0_8' value="Recall 30m Low" />
+            <input type="submit" name='m_0_0_9' value="Recall 30m High" />
+            <input type="submit" name='m_0_1_8' value="Save 30m Low" />
+            <input type="submit" name='m_0_1_9' value="Save 30m High" />
+            </form>             
             </body>
             </html>
             """
@@ -149,11 +217,11 @@ def webpage(temperature, state):
 def serve(connection):
     #Start a web server
     state = 'OFF'
-    temperature = 0
     while True:
         client = connection.accept()[0]
         request = client.recv(1024)
         request = str(request)
+        print('HTTP client request')
         try:
             request = request.split()[1]
         except IndexError:
@@ -175,6 +243,7 @@ def serve(connection):
                 #save_motor_positions(motors[0].counter,motors[1].counter,motors[2].counter)
                 stepper_positions = [motors[0].counter, motors[1].counter, motors[2].counter]
                 nv_data.write_current_steppers( file, stepper_positions )
+                user_display.set_steppers( stepper_positions )
                 #nv_data.write_current_steppers( file, [motors[0].counter,motors[1].counter,motors[2].counter] )
                 
             elif f == 'm':
@@ -182,12 +251,13 @@ def serve(connection):
                 # where s is ignored, d=0 ( recall ) d=1 ( write ), n is memory number
                 if ( d ):
                     #save memory
-                    memories[n].store( motors[0].counter,motors[1].counter,motors[2].counter )
+                    nv_data.write_memory(file, n, [motors[0].counter,motors[1].counter,motors[2].counter])
+                    #memories[n].store( motors[0].counter,motors[1].counter,motors[2].counter )
 
                 else:
                     # recall stepper value for this frequency, workout the change required to go there
                     for each_motor in range(3):
-                        offset = memories[n].recall()[each_motor] - motors[each_motor].counter
+                        offset = nv_data.get_memory(n)[each_motor] - motors[each_motor].counter
                         #for the capacitors, they only should be set 0 - 100 as they are only useful over 180 degrees
                         #and capacitors should not be sent to negative settings - it will work but will only confuse the situation
                         #the inductor ( rollercoaster ) should not try to go to a negative reason. Mechanically it cannot go to a negative position 
@@ -197,20 +267,36 @@ def serve(connection):
                             motors[each_motor].rotate(0,abs(offset))
                             
                         nv_data.write_current_steppers( file, [motors[0].counter,motors[1].counter,motors[2].counter])
-                        #save_motor_positions(motors[0].counter,motors[1].counter,motors[2].counter)
+
                 
         else:
             pass
             #print('name length incorrect')
         
         
-        html = webpage(temperature, state)
+        html = webpage(state)
         client.send(html)
         client.close()
 
 
+button_a.irq(trigger=Pin.IRQ_FALLING,handler=button_a_isr)
+button_b.irq(trigger=Pin.IRQ_FALLING,handler=button_b_isr)
+button_x.irq(trigger=Pin.IRQ_FALLING,handler=button_x_isr)
+button_y.irq(trigger=Pin.IRQ_FALLING,handler=button_y_isr)
+
+# interrupt boot if button_a pressed - when this file has been renamed main.py it is difficult to get out of the loop
+# in order to update software
+
+sleep(5)
+if ( button_a == 0 ) :
+    exit()
+
 file = 'xyzzy.txt'
 nv_data = Storage(file)
+user_display = display_handling.LocalDisplay(nv_data.get_stepper_positions())
+[motors[0].counter, motors[1].counter, motors[2].counter] = nv_data.get_stepper_positions()
+#ser_display.set_steppers(nv_data.get_stepper_positions())
+
 
 try:
     ip=connect()
--- storage.py
+++ storage.py
@@ -8,13 +8,17 @@ import json
 # On initialise the xyzzy.txt file is loaded into RAM containing the current stepper motor locations and memoried
 # locations ( number of steps )
 # The class also handles saving to files for the stepper positions and memories
+# ToDo : make the number of memories and associated memory names variable
+# ToDo : make the number of steppers variable
+# ToDo : make the stepper names configurable
 class Storage:
-    def __init__(self, file ):
+    def __init__(self, file:str ):
         try:
             with open(file) as fd:
                 self.config = json.load(fd)
                 self.steppers = self.config["stepper_stored_pos"]
                 self.memories = self.config["memory"]
+                self.memory_names = self.config["memory_names"]
                 self.ssid = self.config["ssid"]
                 self.password = self.config["password"]
                 fd.close()
@@ -24,26 +28,17 @@ class Storage:
             print(f'Storage file {file} not found, creating a new storage file')
             with open(file, "w") as fd:
                 tmp_stepper_pos = [0,0,0]
-                tmp_create_memories = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],]
+                tmp_create_memories = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+                tmp_create_memory_names = [['160m Low'],['160m High'],['80m Low'],['80m High'],['60m Low'],['60m High'],['40m Low'],['40m High'],['30m Low'],['30m High']]
                 self.config = {
                     "stepper_stored_pos": tmp_stepper_pos,
                     "memory": tmp_create_memories,
+                    "memory_names": tmp_create_memory_names,
                     "ssid": "default",
                     "password": "default",
                 }
                 json.dump(self.config, fd)
                 fd.close()
-
-#     def dump_file( self, file:str ):
-#         with open(file, "w") as fd:
-#             self.config = {
-#                     "stepper_stored_pos": self.steppers,
-#                     "memory": self.memories,
-#                     "ssid": self.ssid,
-#                     "password": self.password,
-#             }
-#             json.dump(self.config, fd)
-#             fd.close()        
 
     def write_memory( self, file:str,  memory_number:int, mem_steppers):
         with open(file, "w") as fd:
@@ -57,11 +52,10 @@ class Storage:
             json.dump(self.config, fd)
             fd.close()         
 
-
     def get_stepper_positions(self):
         return self.config["stepper_stored_pos"] 
 
-    def get_memory(self, memory_number):
+    def get_memory(self, memory_number:str):
         return self.config['memory'][memory_number]
     
     def get_ssid( self ):
@@ -70,29 +64,8 @@ class Storage:
     def get_password( self ):
         return self.config["password"]
  
-    # DEBUG ONLY
-    def dump_file( self ):
-        print('File')
-        with open(file) as fd:
-            self.config = json.load(fd)
-            print ( self.config["stepper_stored_pos"] )
-            print ( self.config["memory"] )
-            print ( self.config["ssid"] )
-            print ( self.config["password"] )
-            fd.close()
-
-    # DEBUG ONLY
-    def dump_memory( self ):
-            print ( self.config["stepper_stored_pos"] )
-            print ( self.config["memory"] )
-            print ( self.config["ssid"] )
-            print ( self.config["password"] )
-            
-            
- 
-
-
 if __name__ == '__main__':
+    # test data :
     # fill the memory space for 5 bands, high and low settings for each 
 
     test_positions1 = [16,250,8]
@@ -105,16 +78,14 @@ if __name__ == '__main__':
     print( f'current stepper positions: {nv_data.get_stepper_positions()}\n')
     
     nv_data.write_memory(file, 3,test_positions1)
-    print( f'memory 3: {nv_data.get_memory(4)}\n')
+    print( f'memory 3: {nv_data.get_memory(3)}\n')
     
     nv_data.write_memory(file, 3,test_positions2)
-    print( f'memory 3: {nv_data.get_memory(4)}\n')
+    print( f'memory 3: {nv_data.get_memory(3)}\n')
     
-    #we don't need to set the password. The user can change the file on the pico to suit their network
+    # We don't need to set the password. The user can change the file on the pico to suit their network
     print( f'SSID: {nv_data.get_ssid()}')
     
-    
-    nv_data.dump_file() 
-    nv_data.dump_memory()
+
 
     
